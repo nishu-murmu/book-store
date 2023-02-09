a660b52ccd0d9448c494afd7a7564581db9b3820
nishu-murmu <nishumurmu017@gmail.com>
2023-02-09 23:58:52 +0530

    fixing types issue in publisher async function

--- client/pages/publisher.tsx
+++ client/pages/publisher.tsx
@@ -1,5 +1,5 @@
 import { Button, Input, HStack, Container, Center, FormControl, FormLabel } from '@chakra-ui/react'
-import { useRef } from 'react'
+import  {useRef , MouseEvent} from 'react'
 
 import Layout from "../components/layout/main"
 
@@ -9,8 +9,10 @@ const Publisher: React.FC = () => {
     const publisherRef = useRef<HTMLInputElement>(null)
     const addressRef = useRef<HTMLInputElement>(null)
 
+    const formRef = useRef<HTMLFormElement>(null)
 
-    const submitHandler = async (): Promise<void> => {
+    const submitHandler = async (e: MouseEvent<HTMLButtonElement>): Promise<void> => {
+        e.preventDefault()
         const publisherDetails: object = {
             publisherName: publisherRef.current?.value,
             address: addressRef.current?.value
@@ -21,22 +23,30 @@ const Publisher: React.FC = () => {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(publisherDetails)
         })
+        reset()
+    }
+
+    const reset = () => {
+        formRef.current?.reset()
     }
     return (<Container>
         <Layout heading='Add Publisher' />
         <Center mt={"40px"}>
-            <FormControl>
-                <FormLabel htmlFor='publishername'>Publisher Name:</FormLabel>
-                <Input type={'text'} ref={publisherRef} id={'publishername'} />
-                <FormLabel htmlFor='address'>Address:</FormLabel>
-                <Input type={'text'} id={'address'} ref={addressRef} />
-                <Center mt={10}>
-                    <HStack spacing={20}>
-                        <Button type='submit' onClick={() => submitHandler()} colorScheme={'green'} >Submit</Button>
-                        <Button type='reset' colorScheme='red'>Reset</Button>
-                    </HStack>
-                </Center>
-            </FormControl>
+            <form ref={formRef} action="#">
+                <FormControl>
+                    <FormLabel htmlFor='publishername'>Publisher Name:</FormLabel>
+                    <Input type={'text'} ref={publisherRef} id={'publishername'} />
+                    <FormLabel htmlFor='address'>Address:</FormLabel>
+                    <Input type={'text'} id={'address'} ref={addressRef} />
+                    <Center mt={10}>
+                        <HStack spacing={20}>
+                            <Button type='submit' onClick={(e) => submitHandler(e)} colorScheme={'green'} >Submit</Button>
+                            <Button type='reset' onClick={() => reset()} colorScheme='red'>Reset</Button>
+                        </HStack>
+                    </Center>
+                </FormControl>
+            </form>
+
         </Center>
     </Container>)
 }
